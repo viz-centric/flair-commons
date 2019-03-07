e7b43c4240e23211f525df586018e2631495792e
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-03-07 20:32:47 +0200

    - added immutable flair query properties - added cache enabled property

--- src/main/java/com/project/bi/query/FlairQuery.java
+++ src/main/java/com/project/bi/query/FlairQuery.java
@@ -1,17 +1,45 @@
 package com.project.bi.query;
 
 
-import lombok.Data;
-
-@Data
 public class FlairQuery {
 
-    /**
-     * Query statement
-     */
     private String statement;
-
     private boolean pullMeta;
-
     private String source;
+    private boolean isCacheEnabled;
+
+    public FlairQuery(String statement, boolean pullMeta) {
+        this.statement = statement;
+        this.pullMeta = pullMeta;
+        this.source = null;
+        this.isCacheEnabled = false;
+    }
+
+    public FlairQuery(String statement, boolean pullMeta, String source, boolean isCacheEnabled) {
+        this.statement = statement;
+        this.pullMeta = pullMeta;
+        this.source = source;
+        this.isCacheEnabled = isCacheEnabled;
+    }
+
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public boolean isPullMeta() {
+        return pullMeta;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public boolean isCacheEnabled() {
+        return isCacheEnabled;
+    }
+
+    public void setCacheEnabled(boolean cacheEnabled) {
+        isCacheEnabled = cacheEnabled;
+    }
 }
