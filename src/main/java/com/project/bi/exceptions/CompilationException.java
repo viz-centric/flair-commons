6cd709f900873df463a7f95391b586856ec05a4e
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-04-04 08:41:06 +0300

    - removed javadoc issue

--- src/main/java/com/project/bi/exceptions/CompilationException.java
+++ src/main/java/com/project/bi/exceptions/CompilationException.java
@@ -45,8 +45,7 @@ public class CompilationException extends FlairCommonsExceptions {
      * message of <tt>(cause==null ? null : cause.toString())</tt> (which
      * typically contains the class and detail message of <tt>cause</tt>).
      * This constructor is useful for exceptions that are little more than
-     * wrappers for other throwables (for example, {@link
-     * PrivilegedActionException}).
+     * wrappers for other throwables.
      *
      * @param cause the cause (which is saved for later retrieval by the
      *              {@link #getCause()} method).  (A <tt>null</tt> value is
