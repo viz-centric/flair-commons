567de9562a1793757831c20a695b55bdb5975799
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-12-19 16:08:44 +0200

    type casting for like condition

--- src/main/java/com/project/bi/query/dto/LikeFeatureNameDTO.java
+++ src/main/java/com/project/bi/query/dto/LikeFeatureNameDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class LikeFeatureNameDTO extends ValueDTO {
+public class LikeFeatureNameDTO {
     private String featureName;
     private String type;
 
