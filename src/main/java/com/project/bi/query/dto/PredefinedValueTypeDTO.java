82d89f75965044454ec650b3fb6cd85ce22f8c6f
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2020-02-15 22:10:54 +0200

    modified predefined value type to support more flair functions

--- src/main/java/com/project/bi/query/dto/PredefinedValueTypeDTO.java
+++ src/main/java/com/project/bi/query/dto/PredefinedValueTypeDTO.java
@@ -7,21 +7,24 @@ import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PredefinedValueTypeDTO extends ValueDTO {
-    public static final Set<String> SUPPORTED_VALUE_TYPES = new HashSet<String>() {{
-        add("__FLAIR_NOW()");
+    public static final Set<Pattern> SUPPORTED_VALUE_TYPES = new HashSet<Pattern>() {{
+        add(Pattern.compile("__FLAIR_[_A-Z]+\\(.*\\)"));
     }};
 
     private String value;
 
     @Override
     public String interpret() {
-        if (SUPPORTED_VALUE_TYPES.contains(value)) {
+        boolean isAllowed = SUPPORTED_VALUE_TYPES.stream()
+                .anyMatch(p -> p.matcher(value).matches());
+        if (isAllowed) {
             return value;
         }
         throw new IllegalArgumentException("Cannot find predefined value type " + value);
